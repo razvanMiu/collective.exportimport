14e06d93d371d528da5ac8ddf27ac8f6010fb2f4
Miu Razvan <miu.razvan28@gmail.com>
2024-07-18 11:02:00 +0300

    update

--- src/collective/exportimport/import_content.py
+++ src/collective/exportimport/import_content.py
@@ -1160,6 +1160,8 @@ class FixCollectionQueries(BrowserView):
 class ImportDeferred(BrowserView):
     def __call__(self):
         # This example reuses the form export_other.pt from collective.exportimport
+        import pdb
+        pdb.set_trace()
         self.title = "Import deferred data"
         self.results = []
         for brain in api.content.find(DEFERRED_FIELD_MAPPING.keys()):
