8f56730ab0a836d0c75d950644d259a52a80bece
Miu Razvan <miu.razvan28@gmail.com>
2024-12-17 21:34:56 +0200

    update

--- src/collective/exportimport/export_other.py
+++ src/collective/exportimport/export_other.py
@@ -1589,6 +1589,9 @@ class ExportReport(ExportEEAContent):
         portal_workflow = getToolByName(
             self.context, "portal_workflow", None)
 
+        import pdb
+        pdb.set_trace()
+
         for o in obj.contentItems():
             if portal_workflow.getInfoFor(
                     o[1], 'review_state') != 'published':
@@ -1810,8 +1813,6 @@ class ExportReport(ExportEEAContent):
 
         children = self.getChildren(obj)
         report_content += self.getFolderContents(children, item)
-        import pdb
-        pdb.set_trace()
 
         for field in self.DISSALLOWED_FIELDS:
             if field in item:
