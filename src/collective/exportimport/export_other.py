ccbcf2caeded7867efa23d049416964bc4fe01d0
Miu Razvan <miu.razvan28@gmail.com>
2024-12-18 22:32:36 +0200

    update

--- src/collective/exportimport/export_other.py
+++ src/collective/exportimport/export_other.py
@@ -1621,8 +1621,7 @@ class ExportReport(ExportEEAContent):
 
     def getFolderContents(self, objects, item):
         new_objects = []
-        import pdb
-        pdb.set_trace()
+
         for index, o in enumerate(objects):
             serializer = getMultiAdapter((o, self.request), ISerializeToJson)
             objects[index] = serializer()
